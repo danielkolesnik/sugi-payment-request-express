8234e145244d6e564078354ceffe45049de492c4
Daniel <workkd97@gmail.com>
2018-12-20 17:13:03 +0200

    [+] changed main file name to index.js according to package.json;

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
-// outsource modules
+// import modules
 var path = require('path');
 var express = require('express');
-var config = require('./environment');
 var mustacheExpress = require('mustache-express');
-// local modules
-var initRouter = require('./router');
 
 // declare global variables
 var app = express();
+
 // setup static resources
 app.use(express.static(path.join(__dirname, 'public')));
-// NOTE setup routing
-initRouter(app);
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
+
+
+app.get('/pay/:walletId', function (request, response) {
+  var amount = request.query.amount,
+    currency = request.query.currency,
+    walletId = request.params.walletId,
+    currencyType = (currency || '').toUpperCase(),
+    data = {},
+    amountValidation = parseFloat(amount) || -1;
+
+  switch( currencyType ) {
+    case 'EUR':
+      data = {
+        walletId: walletId,
+        currencyType: 'eur',
+        amount: amount,
+        currency: currencyType,
+        error: false,
+        errorMessage: ''
+      };
+      break;
+    case 'BTC':
+      data = {
+        currencyType: 'btc',
+        walletId: walletId,
+        amount: amount,
+        currency: currencyType,
+        error: false,
+        errorMessage: ''
+      };
+      break;
+    case 'ETH':
+      data = {
+        currencyType: 'eth',
+        walletId: walletId,
+        amount: amount,
+        currency: currencyType,
+        error: false,
+        errorMessage: ''
+      };
+      break;
+    default:
+      data = {
+        currencyType: 'error',
+        walletId: walletId,
+        amount: amount,
+        currency: currencyType,
+        error: true,
+        errorType: 'CURRENCY'
+      }
+  }
+
+
+  if(amountValidation <= 0) {
+    data = {
+      currencyType: 'error',
+      walletId: walletId,
+      amount: amount,
+      currency: currencyType,
+      error: true,
+      errorType: 'AMOUNT'
+    }
+  }
+
+  // response view using mustache-express
+  response.render('pay', data);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
